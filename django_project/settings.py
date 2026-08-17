c68aa2ba4af1a14450514508d8ec3c398f4ce780
Muhammad-Sada <sadaibrahim126@gmail.com>
2026-08-17 11:05:40 +0100

    Preparing article project for deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles" 
 
 AUTH_USER_MODEL = "accounts.Customuser"
 
