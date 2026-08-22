62ea83e6e08d3e7499e349f478cbaf8bfbfd1ca9
Muhammad-Sada <sadaibrahim126@gmail.com>
2026-08-22 21:54:01 +0100

    more commits for deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -22,15 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+tv)75^_cp80qx&qd=#yh^!!!nx97e)d9^5uxy4mmq3&euvsg-"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-+tv)75^_cp80qx&qd=#yh^!!!nx97e)d9^5uxy4mmq3&euvsg-")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
-).split(",")
+ALLOWED_HOSTS = [
+    os.environ.get("RENDER_EXTERNAL_HOSTNAME", "127.0.0.1"),
+    "localhost",
+]
 
 
 # Application definition
